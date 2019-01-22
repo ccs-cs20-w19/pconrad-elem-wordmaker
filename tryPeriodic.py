cf85997d8d15899a87c8532aa63c6b2b09b4760a
Phill Conrad <pconrad@cs.ucsb.edu>
2019-01-22 13:42:31 -0800

    fix bug where we were not returning a value; thanks Shay

--- tryPeriodic.py
+++ tryPeriodic.py
@@ -2,17 +2,33 @@
 
 import json
 import pprint
+import pytest
 
+def getSymbolList():
+    with open('PeriodicTableJSON.json') as json_data:
+      pt = json.load(json_data)
 
-with open('PeriodicTableJSON.json') as json_data:
-  pt = json.load(json_data)
+    symbolList = []
+    for element in pt['elements']:
+      symbolList.append(element['symbol'])
 
-symbolList = []
-for element in pt['elements']:
-  symbolList.append(element['symbol'])
-  if (len(symbolList)%10==0):
-    print(symbolList)
+    return symbolList
 
+def test_getSymbolList_len():
+   result = getSymbolList()
+   assert len(result)==119
+
+def test_getSymbolList_H():
+   result = getSymbolList()
+   assert "H" in result
+   
+def test_getSymbolList_Li():
+   result = getSymbolList()
+   assert "Li" in result
+      
+   
+
+    
    
    
 
